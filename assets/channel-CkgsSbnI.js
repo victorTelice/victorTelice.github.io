fb272e010a298289e155bf961237ca9bc7544615
victorTelice <vgomez.pascual@telice.com>
2025-07-07 09:19:56 +0200

    movido a la raiz

---
+++ assets/channel-CkgsSbnI.js
@@ -0,0 +1,1 @@
+import{U as s,C as o}from"./mermaid-CF64dWR7.js";const n=(a,r)=>s.lang.round(o.parse(a)[r]);export{n as c};
---
+++ assets/clone-DnjbzbBr.js
@@ -0,0 +1,1 @@
+import{b as n}from"./_baseUniq-DsVx03Xh.js";function o(r){return n(r,4)}export{o as c};
